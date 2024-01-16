b1d5ffc921095e8f5de44b67499a26a152b859ae
rafalkkk <rafal.kraik@gmail.com>
2024-01-16 21:58:53 +0100

    initial

---
+++ check_password.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+query1 = f"SELECT * FROM users WHERE name = '{username}' AND password = '{password}'"
+
+print(f'User validation with {query1}')
+c.execute(query1)
+result = c.fetchone()
+
+if result:
+    print("Login successful")
+else:
+    print("Login failed")
+
+
+query2 = f"SELECT * FROM users WHERE name = ? AND password = ?"
+
+print(f'User validation with {query2}')
+c.execute(query2, (username, password))
+result = c.fetchone()
+
+if result:
+    print("Login successful")
+else:
+    print("Login failed")
+
+        
