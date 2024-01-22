d854b84ecabb9ba5d23c153a0156062c23581e48
rafalkkk <rafal.kraik@gmail.com>
2024-01-22 13:13:53 +0100

    error cleanup

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-"""Main application file"""
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
+# Error list
+# https://bandit.readthedocs.io/en/latest/plugins/index.html#complete-test-plugin-listing
+
 from flask import Flask, url_for, request, redirect
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-def dyn():
-    """Function generates dynamic route"""
-    return f'Dynamically added route<br><a href={ url_for("hello_world") }>take me home</a>'
-app.add_url_rule('/dynamic', 'dynamic_route', dyn)
-
-# The route() function of the Flask class is a decorator
-# which tells the application which URL should call
-# the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
+def start():
     """Function generates hello_world page"""
     content = f'''<html>
       <body>    
-      <h1>6KINGS</h1> 
+      <h1>6-KINGS-CLUB</h1> 
       <form action = { url_for("login") } method = "post">
       <p>Enter Name:</p>
       <p><input type = "text" name = "nm" /></p>
@@ -30,13 +19,13 @@ def hello_world():
       </body>
       </html>
 '''
+    return f'Hello (6) World<br><br>{content}'
 
-    return f'Hello (6) World<br><a href={ url_for("dynamic_route") }>click me</a><br>{content}'
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+# BAD PRACTICE - SILENCED ERROR - B110
     try:
-        """Function shows login page"""
         if request.method == 'POST':
             user = request.form['nm']
             return redirect(url_for('success', name=user))
@@ -45,18 +34,21 @@ def login():
     except:
         pass
 
+
 @app.route('/success/<name>')
 def success(name):
     """Function generates the success message"""
-    return f'welcome {name} <br><a href={ url_for("hello_world") }>take me home</a>'
+    return f'welcome {name} <br><a href={ url_for("start") }>take me home</a>'
 
+
+@app.route('/failure/<name>')
 def failure(name):
     """Function generates the failure message"""
     return f'''Sorry, we don\'t belive you are {name} <br>
-<a href={ url_for("hello_world") }>try again</a>'''
+<a href={ url_for("start") }>try again</a>'''
 
-# main driver function
+
 if __name__ == '__main__':
-	# run() method of Flask class runs the application
-	# on the local development server.
+    # ERROR - FLASK APPLICATION WORKING IN DEBUG MODE - B201
     app.run(debug=True)
+
